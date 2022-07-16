606cd8f47794f81dce841a38d268777d3f2f8340
Levi R <levir620@skiff.com>
2022-07-15 20:39:04 -0700

    ✨ /purge command

--- src/commands/chat/purge.ts
+++ src/commands/chat/purge.ts
@@ -1,46 +1,51 @@
-import { MessageEmbed } from "discord.js";
+import { ApplicationCommandOptionType } from "discord-api-types/v10";
+import { TextChannel } from "discord.js";
 import { ChatCommand, Ctx } from "..";
-import { Permissions } from "../../util/permissions";
-import { accentColour } from "../../main";
-import { ApplicationCommandOptionType } from "discord-api-types/v10"
-import { ChannelTypes } from "discord.js/typings/enums";
 
 class PurgeCommand extends ChatCommand {
-    public constructor() {
-        super("purge", {
-            description: "🔥 Purge messages - (Administrative)",
-            roles: [
+	public constructor() {
+		super("purge", {
+			description: "🔥 Purge messages - (Administrative)",
+			roles: [],
+			args: [
+				{
+					name: "number_of_messages",
+					description: "Number of messages",
+					type: ApplicationCommandOptionType.Integer,
+					required: true,
+				},
+				{
+					name: "from_user",
+					description: "From a specific user",
+					type: ApplicationCommandOptionType.User,
+				},
+				{
+					name: "from_role",
+					description: "From a specific role",
+					type: ApplicationCommandOptionType.Role,
+				},
+				{
+					name: "in_channel",
+					description: "In channel",
+					type: ApplicationCommandOptionType.Channel,
+				},
+			],
+		});
+	}
 
-            ],
-            args: [
-                {
-                    name: "number_of_messages",
-                    description: "Number of messages",
-                    type: ApplicationCommandOptionType.Integer,
-                    required: true
-                },
-                {
-                    name: "from_user",
-                    description: "From a specific user",
-                    type: ApplicationCommandOptionType.User
-                },
-                {
-                    name: "from_role",
-                    description: "From a specific role",
-                    type: ApplicationCommandOptionType.Role
-                },
-                {
-                    name: "in_channel",
-                    description: "In channel",
-                    type: ApplicationCommandOptionType.Channel
-                }
-            ]
-        });
-    }
-
-    public async exec(ctx: Ctx) {
-    
-    }
+	public async exec(ctx: Ctx) {
+		const messageAmount = parseInt(
+			ctx.options.get("number_of_messages")!.value as string
+		) as number;
+		const channel: TextChannel = (await ctx.client.channels.cache.get(
+			ctx.channelId
+		)) as TextChannel;
+		channel.bulkDelete(messageAmount);
+		ctx.reply({
+			content: `🔥 Purged ${messageAmount} messages`,
+			ephemeral: true,
+		});
+	}
 }
 
 export default new PurgeCommand();
